ddf9bd03e515a0113b3767799b4a1f686a83e64f
Pat <ibarretanavarro@gmail.com>
2026-05-07 16:41:43 -0700

    feat: newsletter endpoints

--- src/modules/newsletter/subscribers/subscribers.controller.ts
+++ src/modules/newsletter/subscribers/subscribers.controller.ts
@@ -1,14 +1,24 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
 import { SubscribersService } from './subscribers.service';
 import { AllowAnonymous } from '@thallesp/nestjs-better-auth';
+import { CreateSubscriberDTO } from './dto/create-subscriber.dto';
+import { NewsletterSubscriber } from '@prisma/client';
 
 @Controller('newsletter/subscribers')
 export class SubscribersController {
   constructor(private readonly subscribersService: SubscribersService) {}
 
   @AllowAnonymous()
+  @Post()
+  async create(
+    @Body() body: CreateSubscriberDTO
+  ): Promise<NewsletterSubscriber> {
+    return this.subscribersService.create(body);
+  }
+
+  @AllowAnonymous()
   @Get()
-  findAll() {
-    return 'hello world';
+  async findAll(): Promise<NewsletterSubscriber[]> {
+    return this.subscribersService.findAll();
   }
 }
